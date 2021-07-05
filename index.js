980e6e767a01dfe72b31d429b7916b18443a7024
Shivam Bhatia <shivambhatia1926@gmail.com>
2021-07-05 10:36:29 +0530

    add heroku commands

--- index.js
+++ index.js
@@ -15,6 +15,14 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 app.use("/api", apiRouter);
 
+if (process.env.NODE_ENV == "production") {
+	app.use(express.static("client/build"));
+	const path = require("path");
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
+
 app.listen(apiPort, () => {
 	console.log("server running on 4000");
 });
